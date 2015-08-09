7e5c5fe0d84328189a28cd9a05cdbf3c362823d8
JLoroy <justin@loroy.be>
2015-08-09 15:25:15 +0200

    maj cgdb.sql

--- cgdb.sql
+++ cgdb.sql
@@ -371,4 +371,27 @@ ADD COLUMN `display` INT NOT NULL DEFAULT 1 COMMENT '' AFTER `Categorie_idCatego
 ALTER TABLE `cgdb`.`vendeuse` 
 ADD COLUMN `display` INT NOT NULL DEFAULT 1 COMMENT '' AFTER `Magasin_idMagasin`;
 
-UPDATE commande SET Livraison='2015-08-09 12:00:00' WHERE idCommande < 90;
+UPDATE commande SET Livraison='2015-08-18 12:00:00' WHERE idCommande < 90;
+
+
+CREATE TABLE `cgdb`.`regroupement` (
+  `idRegroupement` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `Nom` LONGTEXT NULL COMMENT '',
+  `Categorie_idCategorie` INT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`idRegroupement`)  COMMENT '',
+  UNIQUE INDEX `idRegroupement_UNIQUE` (`idRegroupement` ASC)  COMMENT '');
+
+ALTER TABLE `cgdb`.`regroupement`
+ADD INDEX `categorie_idx` (`Categorie_idCategorie` ASC)  COMMENT '';
+ALTER TABLE `cgdb`.`regroupement`
+ADD CONSTRAINT `categorie`
+  FOREIGN KEY (`Categorie_idCategorie`)
+  REFERENCES `cgdb`.`categorie` (`idCategorie`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `cgdb`.`regroupement`
+ADD COLUMN `Display` INT UNSIGNED NOT NULL DEFAULT 1 COMMENT '' AFTER `Categorie_idCategorie`;
+
+ALTER TABLE `cgdb`.`produit`
+ADD COLUMN `regroupement_idR
